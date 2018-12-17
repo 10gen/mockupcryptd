0ab97e988c7860e822c709661a7c36babe725b18
Kevin Albertson <kevin.albertson@10gen.com>
2018-12-16 22:50:39 -0500

    Parse schema

--- mockupcryptd/__init__.py
+++ mockupcryptd/__init__.py
@@ -3,32 +3,73 @@ import os
 import sys
 
 from daemon import (DaemonContext, pidfile)
-import mockupdb._bson as bson
+import bson
 from mockupdb import interactive_server
 
 
-def mark_recurse (doc):
+def full_path(path_prefix, key):
+    if path_prefix != "":
+        return path_prefix + "." + key
+    return key
+
+"""
+Given a schema, build a map from a dotted field path to a dictionary with encryption info.
+If a property has an 'encrypt' field, it is considered encrypted.
+This *does not* support encryptMetadata in any way, nor does it recurse into arrays.
+"""
+def build_encrypt_map (map, schema, path_prefix=""):
+    if "properties" in schema:
+        for key, prop in schema["properties"].items():
+            if "encrypt" in prop:
+                encrypt_spec = prop["encrypt"]
+                if "keyId" not in encrypt_spec and "keyAltName" not in encrypt_spec:
+                    raise Exception("encrypt spec must have keyId or keyAltName: {}".format(encrypt_spec))
+                if "keyVaultURI" not in encrypt_spec:
+                    raise Exception("encrypt spec must have keyVaultURI: {}".format(encrypt_spec))
+                if "algorithm" not in encrypt_spec:
+                    raise Exception("encrypt spec must have algorithm: {}".format(encrypt_spec))
+                map[full_path(path_prefix, key)] = encrypt_spec
+            elif "bsonType" in prop and prop["bsonType"] == "object":
+                build_encrypt_map(map, prop, path_prefix=full_path(path_prefix, key))
+
+
+def mark_recurse (doc, encrypt_map, path_prefix=""):
     if isinstance (doc, dict):
         for key in doc:
-            if key == "encryptMe":
-                data = bson.BSON.encode({
-                    "k": "123",
+            path = full_path(path_prefix, key)
+            print("processing {}".format(path))
+            print encrypt_map
+            if path in encrypt_map:
+                # should be encrypted.
+                encrypt_spec = encrypt_map[path]
+                marking = {
                     "v": doc[key],
-                    "iv": bson.Binary(b"\x00\x01\x02\x03\x00\x01\x02\x03\x00\x01\x02\x03\x00\x01\x02\x03")
-                })
-                bin = bson.Binary(data, subtype=7)
-                doc[key] = bin
+                    "a": encrypt_spec["algorithm"],
+                    "u": encrypt_spec["keyVaultURI"]
+                }
+                if "keyId" in encrypt_spec:
+                    marking["k"] = encrypt_map[path]["keyId"]
+                elif "keyAltName" in encrypt_spec:
+                    marking["k"] = encrypt_map[path]["keyAltName"]
+
+                if "iv" in encrypt_spec:
+                    marking["i"] = encrypt_spec["iv"]
+
+                data = bson.BSON.encode(marking)
+                doc[key] = bson.Binary(data, subtype=6)
 
             elif isinstance(doc[key], dict):
-                mark_recurse(doc[key])
+                mark_recurse(doc[key], encrypt_map, path_prefix=full_path(path_prefix, key))
             elif isinstance (doc[key], list):
-                mark_recurse(doc[key])
+                mark_recurse(doc[key], encrypt_map, path_prefix=full_path(path_prefix, key))
     elif isinstance (doc, list):
+        i = 0
         for el in doc:
             if isinstance(el, dict):
-                mark_recurse(el)
+                mark_recurse(el, encrypt_map, path_prefix=full_path(path_prefix, i))
             elif isinstance (el, list):
-                mark_recurse(el)
+                mark_recurse(el, encrypt_map, path_prefix=full_path(path_prefix, i))
+            i+=1
     else:
         raise Exception("Must recurse on list or dict")
 
@@ -45,8 +86,16 @@ def mark_fields(r):
         r.command_err(errmsg="'data' must be array of documents")
         return
 
-    for doc in data:
-        mark_recurse (doc)
+    try:
+        encrypt_map = {}
+        build_encrypt_map(encrypt_map, schema)
+
+        for doc in data:
+            mark_recurse (doc, encrypt_map)
+            print ("result={}".format(doc))
+    except Exception as e:
+        r.command_err(errmsg=str(e))
+        return
 
     logging.info('markFields with {n} documents'.format(n=len(data)))
     r.ok(data=data)
@@ -66,6 +115,8 @@ def start_server():
             try:
                 if r.command_name == 'markFields':
                     mark_fields(r)
+                elif r.command_name == 'hasEncryptedFields':
+                    pass # TODO
                 elif r.command_name == 'shutdown':
                     return
                 else:
@@ -82,6 +133,39 @@ def start_server():
         logging.info('Shutting down')
         server.stop()
 
+def test_schema_parsing():
+    schema = {
+        "bsonType": "object",
+        "properties":
+            {
+                "ssn": {
+                    "encrypt": {
+                        "type": "string",
+                        "algorithm": "Randomized",
+                        "keyId": {
+                            "$binary": {
+                                "base64": "1+niXaxyRL6AB6xRzUp/Ew==",
+                                "subType": "03"
+                            }
+                        },
+                        "keyVaultURI": "mongodb://localhost:27017/admin"
+                    }
+                },
+                "subdoc": {
+                    "bsonType": "object",
+                    "properties": {
+                        "ssn": {
+                            "encrypt": {}
+                        }
+                    }
+                }
+            }
+
+    }
+    map = {}
+    build_encrypt_map(map, schema)
+    for k, v in map.items():
+        print("{} => {}".format(k, v))
 
 def main():
     if len(sys.argv) > 1 and sys.argv[1] == "--daemonize":
